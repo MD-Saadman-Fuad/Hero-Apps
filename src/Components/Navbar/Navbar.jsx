2129a93443d962c39cc4192ca625c90033b0f636
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-10-16 17:54:15 +0600

    navbar and banner complete

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { VscGithub } from "react-icons/vsc";
+import logo from '../../assets/images/logo.png'
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm font-bold">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Home</a></li>
+                        <li>
+                            <a>Apps</a>
+
+                        </li>
+                        <li><a>Installation</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl text-purple-600 font-bold"><img src={logo} className='w-[30px]' alt="" />HERO.IO</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li>
+                        <a href=""> Apps </a>
+                    </li>
+                    <li><a>Installation</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end " >
+                <a className="btn text-white" style={{
+  borderRadius: '4px',
+  background: 'linear-gradient(125.07deg, rgba(99,46,227,1), rgba(159,98,242,1) 100%)'
+}}><VscGithub /> Contribute</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Root from "../Pages/Root/Root";
+import Home from "../Pages/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+        Component: Root,
+    errorElement:<ErrorPage />,
+    children: [
+    {
+      index: true,
+      Component: Home,
+    },
+        
+    ]
+  },
+]);
