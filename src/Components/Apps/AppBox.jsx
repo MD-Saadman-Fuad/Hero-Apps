1b77df9645cd33962a31dfc919955e1c0f514f8f
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-10-16 23:56:11 +0600

    Home Page Design done

---
+++ src/Components/Apps/AppBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import AppCard from './AppCard';
+const AppBox = () => {
+    const appsPromise = fetch('./home.json').then(res => res.json()).then(data => data);
+    console.log(appsPromise);
+    const [apps, setApps] = useState([]);
+
+    useEffect(() => {
+        appsPromise.then(data => {
+            setApps(data);
+        });
+    }, [appsPromise]);
+
+    return (
+        <div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 max-w-[1440px] mx-auto gap-2'>
+                {apps.map(app => <AppCard key={app.id} app={app} />)}
+            </div>
+            <button className="btn mt-2 text-white" style={{
+                borderRadius: '4px',
+                background: 'linear-gradient(125.07deg, rgba(99,46,227,1), rgba(159,98,242,1) 100%)'
+            }}>Show All</button>
+        </div>
+
+    );
+};
+
+export default AppBox;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import App from '../../Components/Apps/App';
 const Home = () => {
     return (
         <div>
             <Banner />
+            <App></App>
+
         </div>
     );
 };
